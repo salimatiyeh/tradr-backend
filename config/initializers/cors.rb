8913c2dfa7b4d46113ea9a85399b7c9c1444db60
Salim Atiyeh <salimatiyeh@gmail.com>
2025-07-25 10:28:15 -0500

    fixed login registration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,9 +14,17 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'http://localhost:4200' # Angular dev server
+#     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
+#   end
+# end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200' # Angular dev server
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    origins '*'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :delete, :patch, :put, :options]
   end
 end
